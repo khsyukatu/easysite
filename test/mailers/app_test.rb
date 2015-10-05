b8382f34a4308c28ef3ff17692fc69bdc1b09522
khsyukatu <higuchi@unistyleinc.com>
2015-10-05 22:04:25 +0000

    rest-client install because rest_client is deleted from rubygems

---
+++ test/mailers/app_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AppTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/mailers/previews/app_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/app
+class AppPreview < ActionMailer::Preview
+
+end
