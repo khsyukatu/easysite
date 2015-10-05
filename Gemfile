ed99813a6f459a77ae575cc8214e6aa5ec81fc51
khsyukatu <higuchi@unistyleinc.com>
2015-10-05 06:16:01 +0000

    subdomain logic deleted

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '4.2.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', group: :development
 gem 'pg', group: :production
-gem 'rails_12factor', group: :production
+gem 'rails_12factor', group: :production #herokuでエラーを吐き出すためのgem
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,10 +1,6 @@
 class SitesController < ApplicationController
   def show
-    if request.subdomain.present? && request.subdomain != 'techacademy-khsyukatu' #'www'に変更すること
-      @site = Site.friendly.find(request.subdomain)
-    else
-      @site = Site.friendly.find(params[:id])
-    end
+    @site = Site.friendly.find(params[:id])
     
     @topimage = @site.topimage
     @service = @site.service
